d2369e84cc62155776203f68daaf6cea05e8ccca
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-04-24 19:49:45 -0300

    Add files via upload

---
+++ openSqlOne.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import sqlite3
+
+# Conecta ao banco de dados SQLite
+conn = sqlite3.connect(r'C:\Users\ACER\Desktop\streamlit\bancoDatas.db')
+cursor = conn.cursor()
+
+# Realiza uma consulta
+cursor.execute("SELECT * FROM 'bancoDatas'")
+units = cursor.fetchall()
+
+# Mostra os resultados na interface Streamlit
+st.subheader("Informações no banco de dados")
+st.write("Unidades:")
+for unit in units:
+    st.write(len(unit))
+    st.write(f" - {unit[0]}")
+    st.write(f" - {unit[1]}")
+    st.write(f" - {unit[2]}")
